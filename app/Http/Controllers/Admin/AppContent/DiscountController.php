ecc5e062c40609965d06bf2c976fee5f91c293df
hmikki <30619974+hmikki@users.noreply.github.com>
2022-09-08 15:20:50 +0300

    u

--- app/Http/Controllers/Admin/AppContent/DiscountController.php
+++ app/Http/Controllers/Admin/AppContent/DiscountController.php
@@ -42,7 +42,7 @@ class DiscountController extends Controller
                 'relation'=>[
                     'data'=> Category::all(),
                     'custom'=>function($Object){
-            return ($Object->getName());
+            return ($Object);
 //                        return app()->getLocale() == 'ar'?$Object->getNameAr():$Object->getName();
                     },
                     'entity'=>'category'

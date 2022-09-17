74c28bafe0499a82c8e963e08e2e1f5704c72897
hmikki <30619974+hmikki@users.noreply.github.com>
2022-09-17 11:58:40 +0300

    u

--- app/Http/Controllers/Admin/AppContent/AdvertisementController.php
+++ app/Http/Controllers/Admin/AppContent/AdvertisementController.php
@@ -15,7 +15,7 @@ class AdvertisementController extends Controller
     public function setup()
     {
         $this->setRedirect('app_content/advertisements');
-        $this->setEntity((new Advertisement())->orderBy('order', 'desc'));
+        $this->setEntity((new Advertisement())->orderBy('created_at', 'desc'));
         $this->setTable('Advertisements');
         $this->setLang('Advertisement');
         $this->setColumns([

cc0679ad09623a2d51369c560810de6d2465b347
hmikki <30619974+hmikki@users.noreply.github.com>
2022-09-22 15:59:29 +0300

    up

--- resources/views/Admin/AppData/Setting/edit2.blade.php
+++ resources/views/Admin/AppData/Setting/edit2.blade.php
@@ -78,7 +78,7 @@
     </div>
 
 @endsection
-@push('script')
+@section('script')
     <link href="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote.min.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote.min.js"></script>
     <script>
@@ -108,4 +108,4 @@
         }
         permissionCheck();
     </script>
-@endpush
+@endsection

76a5d5575540615db808ec6c365bd6e1f577c6c4
hmikki <30619974+hmikki@users.noreply.github.com>
2022-09-29 11:35:12 +0300

    u

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -18,7 +18,7 @@
         <header>
             <nav id="navbar" class="navbar navbar-expand-lg navbar-light">
                 <div class="container-fluid">
-                    <a class="navbar-brand" href="{{url('/')}}"><img src="{{asset('assets/logo.png')}}" alt="" width="100px" height="70px"></a>
+                    <a class="navbar-brand" href="{{url('/')}}"><img src="{{asset('logo.png')}}" alt="" width="100px" height="70px"></a>
                     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span class="navbar-toggler-icon"></span>
                     </button>

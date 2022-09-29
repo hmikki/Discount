d5b84d1c93b45866546169b075717ed4887d59e3
hmikki <30619974+hmikki@users.noreply.github.com>
2022-09-29 11:36:08 +0300

    u

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="ar">
 <head>
-    <title>Shamel @yield('title')</title><!-- site title-->
+    <title>CodeKhasim @yield('title')</title><!-- site title-->
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" type="text/css" href="{{asset('lib/css/all.min.css')}}"><!-- font awesome-->
